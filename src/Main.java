830e5a4725b02b568f73780cf8fd99ad73a1ab12
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-10-02 18:41:59 -0400

    Final commit and push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter your age: ");
+
+        int age = in.nextInt();
+
+        if (age >= 21) {
+            System.out.println("You get a paper wrist band!");
+        }
+
+
     }
 }
